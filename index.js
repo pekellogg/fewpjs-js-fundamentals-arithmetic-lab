146b332a9d990a944b9a383047b77ba1cd9fc32e
Peyton Kellogg <pekellogg@hotmail.com>
2022-08-22 16:29:05 -0700

    passing lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2; // 62
 
-// Write your code here
+const random = Math.floor(Math.random() * 0) + 1; // random number between 0 and 1
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+
+const max = Math.max( 1, 20 );
